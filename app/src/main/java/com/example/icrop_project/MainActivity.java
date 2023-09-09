a603738d3677b5ffd7e6b4d6c7fbf570b6cc680d
Lloyd Adrian Objero <lloydadrianobjero@gmail.com>
2023-09-10 03:19:30 +0800

    Added firebase to gradle

---
+++ app/src/main/java/com/example/icrop_project/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.icrop_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
