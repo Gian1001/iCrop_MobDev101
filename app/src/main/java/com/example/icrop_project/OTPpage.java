f1d9d6ad4a33ff9468ed08374615607f15dc32a2
Lloyd Adrian Objero <lloydadrianobjero@gmail.com>
2023-09-14 14:47:59 +0800

    Updated registration form/OTP page

--- app/src/main/java/com/example/icrop_project/OTPpage.java
+++ app/src/main/java/com/example/icrop_project/OTPpage.java
@@ -1,9 +1,16 @@
 package com.example.icrop_project;
 
+import static com.example.icrop_project.Registration.generateUserId;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,6 +20,7 @@ import org.json.JSONObject;
 
 import com.example.icrop_project.SendMessageTask;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.IOException;
@@ -29,6 +37,12 @@ public class OTPpage extends AppCompatActivity {
     TextView textbox;
     private Button button;
     int currentHour, currentMinute, currentSecond;
+    private boolean isButtonPressed = false;
+
+    private EditText[] editTexts = new EditText[6];
+    private int currentEditText = 0;
+
+
 
     Calendar calendar = Calendar.getInstance();
 
@@ -38,20 +52,42 @@ public class OTPpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otppage);
 
-        textbox = findViewById(R.id.testBox);
+
+
         button = findViewById(R.id.testButton);
         currentHour = calendar.get(Calendar.HOUR_OF_DAY); // 24-hour format
         currentMinute = calendar.get(Calendar.MINUTE);
         currentSecond = calendar.get(Calendar.SECOND);
 
+        editTexts[0] = findViewById(R.id.firstNumber);
+        editTexts[1] = findViewById(R.id.secondNumber);
+        editTexts[2] = findViewById(R.id.thirdNumber);
+        editTexts[3] = findViewById(R.id.fourthNumber);
+        editTexts[4] = findViewById(R.id.fifthNumber);
+        editTexts[5] = findViewById(R.id.sixthNumber);
 
+        // Attach TextWatcher to each EditText
+        for (int i = 0; i < editTexts.length; i++) {
+            attachTextWatcher(editTexts[i], i);
+        }
+
+        // Set an onClickListener for the "test button"
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SendMessageTask sendMessageTask = new SendMessageTask();
-                sendMessageTask.execute(generateOTP());
+                isButtonPressed = true;  // Change the value of the boolean variable
+
+                // Create an intent to pass the value back to the registration page
+                Intent intent = new Intent();
+                intent.putExtra("otpVerification", isButtonPressed);
+
+                // Set the result code and the intent with data
+                setResult(RESULT_OK, intent);
+
+                // Close the OTP page and go back to the registration page
+                finish();
             }
         });
 
@@ -70,14 +106,26 @@ public class OTPpage extends AppCompatActivity {
         return otp.toString();
     }
 
-    private void insertData() {
+    private void attachTextWatcher(final EditText editText, final int index) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
-//        FirebaseDatabase.getInstance().getReference().child("crops_db").setValue().addOnSuccessListener(new OnSuccessListener<Void>() {
-//            @Override
-//            public void onSuccess(Void unused) {
-//
-//            }
-//
-//        });
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // When a digit is entered, automatically focus on the next EditText
+                if (editable.length() == 1 && index < editTexts.length - 1) {
+                    currentEditText = index + 1;
+                    editTexts[currentEditText].requestFocus();
+                }
+            }
+        });
     }
 }
+
+
